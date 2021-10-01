ec5c407b5fb71e13e68ca259cb9470d27eaa7c5d
lo-sam <75124382+lo-sam@users.noreply.github.com>
2021-10-01 11:37:33 +0200

    cryptage mail

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,13 +1,19 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
+const AES = require("crypto-js/aes");
+const hmac = require('crypto-js/hmac-sha256');
 
+const key = "tokenMail";
 
 exports.signup = (req, res, next) => {
+    let encrypted = CryptoJS.SHA256(req.body.email, key).toString();
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
+
             const user = new User({
-                email: req.body.email,
+                email: encrypted,
                 password: hash
             });
             user.save()
@@ -18,8 +24,12 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
+
+    let encrypted = CryptoJS.SHA256(req.body.email, key).toString();
+    User.findOne({ email: encrypted }) //req.body.email })
+
+    .then(user => {
+
             if (!user) {
                 return res.status(401).json({ error: 'Utilisateur non trouvé !' });
             }
@@ -35,7 +45,8 @@ exports.login = (req, res, next) => {
                         )
                     });
                 })
-                .catch(error => res.status(500).json({ error }));
+                .catch(error => res.status(500).json({ error: 'Impossible de vérifier si le mot de pass!' }));
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(500).json({ error: 'Impossible de vérifier si l\'utilisateur existe!' }));
+
 };
